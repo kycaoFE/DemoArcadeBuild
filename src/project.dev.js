c6a9f7afd1682d0fe35b0ae0d7f1a96507667353
kycaoFE <ky.cao@enotion.io>
2024-01-18 10:43:35 +0700

    Scale Screen

--- src/project.dev.js
+++ src/project.dev.js
@@ -38145,6 +38145,70 @@ window.__require = function e(t, n, r) {
     lodash: 157,
     utils: "utils"
   } ],
+  gameResize: [ function(require, module, exports) {
+    "use strict";
+    cc._RF.push(module, "f0052T/CM9DPJPxa6ChdZNV", "gameResize");
+    "use strict";
+    var __extends = this && this.__extends || function() {
+      var extendStatics = function(d, b) {
+        extendStatics = Object.setPrototypeOf || {
+          __proto__: []
+        } instanceof Array && function(d, b) {
+          d.__proto__ = b;
+        } || function(d, b) {
+          for (var p in b) b.hasOwnProperty(p) && (d[p] = b[p]);
+        };
+        return extendStatics(d, b);
+      };
+      return function(d, b) {
+        extendStatics(d, b);
+        function __() {
+          this.constructor = d;
+        }
+        d.prototype = null === b ? Object.create(b) : (__.prototype = b.prototype, new __());
+      };
+    }();
+    var __decorate = this && this.__decorate || function(decorators, target, key, desc) {
+      var c = arguments.length, r = c < 3 ? target : null === desc ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+      if ("object" === typeof Reflect && "function" === typeof Reflect.decorate) r = Reflect.decorate(decorators, target, key, desc); else for (var i = decorators.length - 1; i >= 0; i--) (d = decorators[i]) && (r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r);
+      return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+    var CanvasScaleByOrientation = require("CanvasScaleByOrientation");
+    var _a = cc._decorator, ccclass = _a.ccclass, property = _a.property;
+    var NewClass = function(_super) {
+      __extends(NewClass, _super);
+      function NewClass() {
+        var _this = null !== _super && _super.apply(this, arguments) || this;
+        _this.canvas = null;
+        return _this;
+      }
+      NewClass.prototype.start = function() {
+        this.onGameShow();
+      };
+      NewClass.prototype.update = function(dt) {
+        this._visibleSize.equals(cc.view.getVisibleSize()) || this.scaleCanvasByOrientation();
+      };
+      NewClass.prototype.onGameShow = function() {
+        if (cc.sys.isMobile) {
+          var eventResize = new Event("gameShow");
+          window.dispatchEvent(eventResize);
+        }
+      };
+      NewClass.prototype.scaleCanvasByOrientation = function() {
+        _super.prototype.scaleCanvasByOrientation.call(this);
+      };
+      __decorate([ property(cc.Canvas) ], NewClass.prototype, "canvas", void 0);
+      NewClass = __decorate([ ccclass ], NewClass);
+      return NewClass;
+    }(CanvasScaleByOrientation);
+    exports.default = NewClass;
+    cc._RF.pop();
+  }, {
+    CanvasScaleByOrientation: "CanvasScaleByOrientation"
+  } ],
   globalAnimationLibrary: [ function(require, module, exports) {
     "use strict";
     cc._RF.push(module, "59f07eK2q5E9o8QC+2nQTvq", "globalAnimationLibrary");
@@ -41684,5 +41748,5 @@ window.__require = function e(t, n, r) {
   }, {
     "./crypto-core": "crypto-core"
   } ]
-}, {}, [ "betItemController", "betStateManager", "buffaloController", "gaAvatarProvider", "gaButtonFullScreen", "gaCurrencyCalculator", "gaDataFactory", "gaDataStore", "gaEventEmitter", "gaGameView", "gaGeneralInitialization", "gaLocalStorage", "gaLocalize", "gaMainController", "gaReferenceManager", "gaResizeWindow", "gaSafeArea", "gaVersion", "gaCountPoint", "gaRefreshWidget", "gaFBFParticle", "gaArcadeBaseTableHistory", "gaJackpotFrame", "gaNotifyComponent", "gaNotifyController", "gaNotifyJackpot", "gaNotifyJackpotItem", "gaNotifyMessage", "gaBetHistoryItem", "gaHistoryItem", "gaJackpotHistoryItem", "gaBasePopup", "gaDialog", "gaMultipagePopup", "gaPopupBetHistory", "gaPopupController", "gaPopupHelpInfo", "gaPopupHistory", "gaPopupJackpotHistory", "gaPopupSetting", "gaToast", "gaRoundInfo", "gaItemsScroller", "gaScrollItem", "gaSideMenu", "gaToggleDoubleCheckmark", "gaUserUI", "gaWallet", "gaComponent", "gaBaseConfig", "gaMapKeyConfig", "gaCommon", "gaDemoPlayer", "gaEventsCode", "gaGameText", "gaUserInfo", "gaLoadingScene", "gaResultData", "gaRoundData", "gaCommandID", "gaGameNetwork", "gaGameService", "gaISocketManager", "gaNetworkError", "gaSocketManager", "gaCustomDataType", "gaIStateMachine", "gaStateMachine", "gaStateView", "gaLogger", "gaPromise", "gaRandom", "gaScreenUtils", "gaTweenUtils", "gaUtils", "authenPusher", "connectNetwork", "game-network", "globalNetwork", "connectNetworkV3", "globalNetworkV3", "CustomHTMLTextParser", "CustomRichText", "MonoUpdate", "jsb_runtime_md5", "BaseCellHistory", "BaseHistory", "BaseTableHistory", "BetCellHistory", "BetTableHistory", "JackpotCellHistory", "JackpotHistory", "JackpotTableHistory", "ActionEffect", "AlignFullScreenButton", "AlignmentControl", "AnimUtils", "CacheChildrenRender", "ClickAndShow", "CustomDataType", "DisableMultiTouchButtons", "EventListenerManager", "InitSocketComponent", "MoneyDataStore", "PatchIOS14", "ScrollViewOptimize", "SoundPlayer", "ToastInfo", "WalletTrial", "WebSoundPlayer", "_FirstScene", "BaseDirectorV2", "baseActor", "baseAssetsManager", "baseDirector", "big", "configParticle", "crypto-core", "hashKey", "md5", "sha1", "sha224", "sha256", "sha512", "x64-core", "floatUtils", "ControlBtn", "DimFocusControl", "gameCommonUtils", "globalAnimationLibrary", "globalMiniGameState", "globalState", "turnBaseFSM", "utils", "ActorDialogSlot", "AppearCenterScreen", "AutoScaleBackground", "ButtonScaler", "CanvasScale", "CanvasScaleByOrientation", "CircleMove", "InfoText", "JoinGame", "MakePersistent", "MobileDetectLoad", "PlayTrialButtonControl", "ScaleAlignViewport", "SymbolFXController", "animateNumberLabel", "animateNumberZoom", "animatePopup", "app", "betCard", "dragAndDrop", "spineAtlasHolder", "toast", "toggle", "DissolveBlurControl", "DissolveControl", "RadientBlurControl", "FluxaySuper", "Gradient", "Saoguang", "dizzyness", "iTimeUpdate", "lightEffect", "lightsweep", "motionBlur", "scrollUV", "vintageFadeIn", "vintageFadeOut", "XOCypher", "loadConfigAsync", "serviceRest", "data", "layerController", "loadingController", "mEmiter", "mainController", "network", "racingController", "scrollController", "sendMessage", "skyController", "uiManager", "appConfig", "mock" ]);
+}, {}, [ "betItemController", "betStateManager", "buffaloController", "gaAvatarProvider", "gaButtonFullScreen", "gaCurrencyCalculator", "gaDataFactory", "gaDataStore", "gaEventEmitter", "gaGameView", "gaGeneralInitialization", "gaLocalStorage", "gaLocalize", "gaMainController", "gaReferenceManager", "gaResizeWindow", "gaSafeArea", "gaVersion", "gaCountPoint", "gaRefreshWidget", "gaFBFParticle", "gaArcadeBaseTableHistory", "gaJackpotFrame", "gaNotifyComponent", "gaNotifyController", "gaNotifyJackpot", "gaNotifyJackpotItem", "gaNotifyMessage", "gaBetHistoryItem", "gaHistoryItem", "gaJackpotHistoryItem", "gaBasePopup", "gaDialog", "gaMultipagePopup", "gaPopupBetHistory", "gaPopupController", "gaPopupHelpInfo", "gaPopupHistory", "gaPopupJackpotHistory", "gaPopupSetting", "gaToast", "gaRoundInfo", "gaItemsScroller", "gaScrollItem", "gaSideMenu", "gaToggleDoubleCheckmark", "gaUserUI", "gaWallet", "gaComponent", "gaBaseConfig", "gaMapKeyConfig", "gaCommon", "gaDemoPlayer", "gaEventsCode", "gaGameText", "gaUserInfo", "gaLoadingScene", "gaResultData", "gaRoundData", "gaCommandID", "gaGameNetwork", "gaGameService", "gaISocketManager", "gaNetworkError", "gaSocketManager", "gaCustomDataType", "gaIStateMachine", "gaStateMachine", "gaStateView", "gaLogger", "gaPromise", "gaRandom", "gaScreenUtils", "gaTweenUtils", "gaUtils", "authenPusher", "connectNetwork", "game-network", "globalNetwork", "connectNetworkV3", "globalNetworkV3", "CustomHTMLTextParser", "CustomRichText", "MonoUpdate", "jsb_runtime_md5", "BaseCellHistory", "BaseHistory", "BaseTableHistory", "BetCellHistory", "BetTableHistory", "JackpotCellHistory", "JackpotHistory", "JackpotTableHistory", "ActionEffect", "AlignFullScreenButton", "AlignmentControl", "AnimUtils", "CacheChildrenRender", "ClickAndShow", "CustomDataType", "DisableMultiTouchButtons", "EventListenerManager", "InitSocketComponent", "MoneyDataStore", "PatchIOS14", "ScrollViewOptimize", "SoundPlayer", "ToastInfo", "WalletTrial", "WebSoundPlayer", "_FirstScene", "BaseDirectorV2", "baseActor", "baseAssetsManager", "baseDirector", "big", "configParticle", "crypto-core", "hashKey", "md5", "sha1", "sha224", "sha256", "sha512", "x64-core", "floatUtils", "ControlBtn", "DimFocusControl", "gameCommonUtils", "globalAnimationLibrary", "globalMiniGameState", "globalState", "turnBaseFSM", "utils", "ActorDialogSlot", "AppearCenterScreen", "AutoScaleBackground", "ButtonScaler", "CanvasScale", "CanvasScaleByOrientation", "CircleMove", "InfoText", "JoinGame", "MakePersistent", "MobileDetectLoad", "PlayTrialButtonControl", "ScaleAlignViewport", "SymbolFXController", "animateNumberLabel", "animateNumberZoom", "animatePopup", "app", "betCard", "dragAndDrop", "spineAtlasHolder", "toast", "toggle", "DissolveBlurControl", "DissolveControl", "RadientBlurControl", "FluxaySuper", "Gradient", "Saoguang", "dizzyness", "iTimeUpdate", "lightEffect", "lightsweep", "motionBlur", "scrollUV", "vintageFadeIn", "vintageFadeOut", "XOCypher", "loadConfigAsync", "serviceRest", "data", "gameResize", "layerController", "loadingController", "mEmiter", "mainController", "network", "racingController", "scrollController", "sendMessage", "skyController", "uiManager", "appConfig", "mock" ]);
 //# sourceMappingURL=project.dev.js.map
